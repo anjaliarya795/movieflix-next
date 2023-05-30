c8569d947aa60012abf2d41865cf788e7c9448d9
Anjali Arya <anjalisinha498@gmail.com>
2023-05-30 23:00:12 +0530

    Navbar and herosection

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './comp/Navbar';
+import Herosection from './comp/Herosection';
+
+const Page = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Herosection/>
+    </div>
+  );
+}
+
+export default Page;
